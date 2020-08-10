09bf7d1737e0c82bb154f214c933fae98d0333e8
duhongbo <bofearless@gmail.com>
2020-08-10 16:04:56 +0800

    update.

--- DDLocationTest/ViewController.m
+++ DDLocationTest/ViewController.m
@@ -21,11 +21,24 @@
     CGFloat scrennWidth = [UIScreen mainScreen].bounds.size.width;
     CGFloat scrennHeight = [UIScreen mainScreen].bounds.size.height;
     
+    if (@available(iOS 13.0, *)) {
+        self.view.backgroundColor = [UIColor systemBackgroundColor];
+    }else {
+        self.view.backgroundColor = [UIColor cyanColor];
+    }
+    
     UILabel *label = [[UILabel alloc] init];
     label.frame = CGRectMake(0, scrennHeight/2-50, scrennWidth, 30);
     label.font = [UIFont systemFontOfSize:18];
     label.textAlignment = NSTextAlignmentCenter;
-    label.textColor = [UIColor blackColor];
+    
+    if (@available(iOS 13.0, *)) {
+        label.textColor = [UIColor labelColor];
+    } else {
+        // Fallback on earlier versions
+        label.textColor = [UIColor grayColor];
+    }
+    
     label.text = @"开启钉钉虚拟定位";
     
     [self.view addSubview:label];
